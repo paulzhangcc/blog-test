12cef9bf866f66a20032dfadc618b6274845690e
paulzhangcc <1003681427@qq.com>
2019-09-12 10:14:22 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  base: '/blog-test/',
+  title: '张晋峰',
+  description: '张晋峰的博客',
+  themeConfig: {
+	// 你的GitHub仓库，请正确填写
+	repo: 'https://github.com/paulzhangcc',
+	// 自定义仓库链接文字。
+	repoLabel: 'paulzhangcc',
+	nav: [
+		{ text: 'Home', link: '/' },
+		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+	],
+	sidebar: [
+      ['/', '首页'],
+      ['/blog/FirstBlog.md', '我的第一篇博客']
+    ],
+}
+}
